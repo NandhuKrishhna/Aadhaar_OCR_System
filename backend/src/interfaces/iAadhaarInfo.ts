995c519fe1baf1ff710e932d8832a3457b969ed8
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-11 18:55:22 +0530

    feat: add AadhaarInfo interface for user data representation

---
+++ backend/src/interfaces/iAadhaarInfo.ts
@@ -0,0 +1,8 @@
+export default interface AadhaarInfo {
+    dob: string | null;
+    aadhaarNumber: string | null;
+    gender: string | null;
+    name: string | null;
+    address: string | null;
+    mobileNumber: string | null
+}
