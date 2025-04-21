a7b784f683126d903fc1a713b7a51951a9f7b832
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-21 20:58:03 +0530

    feat: add test endpoint to verify API functionality and CORS configuration

--- backend/src/app/routes/routes.ts
+++ backend/src/app/routes/routes.ts
@@ -6,6 +6,10 @@ import upload from "../../utils/multer";
 const ocrController = new OcrController();
 
 const router = Router();
+router.get("/test", (req, res) => {
+    res.send("API is working and CORS is allowed");
+});
+
 router.post("/parsse-aadhar", upload.fields([{ name: 'frontImage' }, { name: "backImage" }]), ocrController.postAadhaar)
 
 export default router;
