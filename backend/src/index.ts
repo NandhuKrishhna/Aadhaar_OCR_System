a9b132e91a621461fc4ed010dd2bc0937f1d80ee
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-21 20:49:58 +0530

    refactor: restructure server setup and middleware configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,45 @@
-import App from "./app";
-import { PORT } from "./utils/env";
+import express, { Application, Request, Response } from "express";
+import http from "http";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import logger from "morgan";
+import route from "./app/routes/routes";
+import { limiter } from "./utils/rateLimiter";
+import errorHandler from "./app/errorHandler";
+import { FRONTEND_URL, PORT } from "./utils/env";
 
-const port = Number(PORT)
-const app = new App();
+const app: Application = express();
+const server = http.createServer(app);
 
-app.startServer(port)
+
+const corsOptions = {
+    origin: 'https://aadhaar-ocr-system-bq61.vercel.app',
+    credentials: true,
+};
+
+
+app.use(express.json({ limit: "50mb" }));
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(logger("dev"));
+app.use(cookieParser());
+app.use(limiter);
+
+
+app.use("/api", route);
+
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({
+        status: "fail",
+        message: `Cannot ${req.method} ${req.originalUrl}`,
+    });
+});
+
+app.use(errorHandler);
+
+const port = Number(PORT);
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
