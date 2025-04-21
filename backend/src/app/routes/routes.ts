28993e35edc30e7fddb63f372c979d9b3f1480e4
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-21 21:38:14 +0530

    feat: implement main application structure with middleware, routes, and error handling

--- backend/src/app/routes/routes.ts
+++ backend/src/app/routes/routes.ts
@@ -2,14 +2,10 @@ import { Router } from "express";
 import { OcrController } from "../controllers/ocr-Controller";
 import upload from "../../utils/multer";
 
-
 const ocrController = new OcrController();
 
 const router = Router();
-router.get("/test", (req, res) => {
-    res.send("API is working and CORS is allowed");
-});
 
-router.post("/parsse-aadhar", upload.fields([{ name: 'frontImage' }, { name: "backImage" }]), ocrController.postAadhaar)
+router.post("/parsse-aadhar", upload.fields([{ name: 'frontImage' }, { name: "backImage" }]), ocrController.postAadhaar);
 
 export default router;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,8 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import helmet from "helmet";
-import logger from "morgan";
-import route from "./app/routes/routes";
-import { limiter } from "./utils/rateLimiter";
-import errorHandler from "./app/errorHandler";
-import { FRONTEND_URL } from "./utils/env";
 
-const app: Application = express();
+import App from "./app";
+import { PORT } from "./utils/env";
 
-app.use(express.json({ limit: "50mb" }));
-app.use(helmet());
-app.use(logger("dev"));
-app.use(cookieParser());
-app.use(limiter);
+const port = Number(PORT)
+const app = new App();
 
-app.use(
-    cors({
-        origin: FRONTEND_URL,
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-);
-
-app.options("*", cors());
-
-app.use("/api", route);
-
-app.use((req: Request, res: Response) => {
-    res.status(404).json({
-        status: "fail",
-        message: `Cannot ${req.method} ${req.originalUrl}`,
-    });
-});
-
-app.use(errorHandler);
-
-export default app;
+app.startServer(port)
