d4b16044cfb8debd3bfcaf08bb7b8fddfbf548a3
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-21 21:47:54 +0530

    refactor: update server export to handle requests directly

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
-
 import App from "./app";
 import { PORT } from "./utils/env";
-
+import { Request, Response, } from "express";
 const port = Number(PORT)
 const app = new App();
 
-app.startServer(port)
+export default (req: Request, res: Response,) => {
+    app.app(req, res);
+};
