4159ae97a3f5461122be81184ebf0114d654057b
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-22 07:12:50 +0530

    fix: update CORS configuration to allow all origins

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ import errorHandler from "./app/errorHandler";
 class App {
     public app: Application;
     public corsOptions = {
-        origin: FRONTEND_URL,
+        origin: "*",
         credentials: true,
     };
 
@@ -25,11 +25,12 @@ class App {
 
     private applyMiddleware(): void {
         this.app.use(express.json({ limit: "50mb" }));
-        this.app.options("*", cors(this.corsOptions));
+        this.app.use(cors(this.corsOptions));
         this.app.use(helmet());
         this.app.use(logger("dev"));
         this.app.use(cookieParser());
         this.app.use(limiter);
+        this.app.options("*", cors(this.corsOptions));
     }
 
     private registerRoutes(): void {
