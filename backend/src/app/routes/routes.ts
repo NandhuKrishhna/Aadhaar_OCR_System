900db5415891bc5ad4a6d0d83c39f3877ca7f3c9
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-22 11:34:06 +0530

    remove test route

--- backend/src/app/routes/routes.ts
+++ backend/src/app/routes/routes.ts
@@ -5,9 +5,6 @@ import upload from "../../utils/multer";
 const ocrController = new OcrController();
 
 const router = Router();
-router.get("/test", (req, res) => {
-    res.status(200).json({ message: "Hello from OCR API" });
-});
 router.post("/parsse-aadhar", upload.fields([{ name: 'frontImage' }, { name: "backImage" }]), ocrController.postAadhaar);
 
 export default router;
