be6e1c0da1f153166d35de2f96d54ad5922462ea
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-22 11:11:11 +0530

    refactor : Index .ts (cors bug)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,39 @@
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { FRONTEND_URL, NODE_ENV, PORT } from "./utils/env";
+import route from "./app/routes/routes";
+import { limiter } from "./utils/rateLimiter";
+import errorHandler from "./app/errorHandler";
 
-import App from "./app";
-import { PORT } from "./utils/env";
+const app = express();
 
-const port = Number(PORT)
-const app = new App();
+// Middleware
+app.use(cors({
+    origin: FRONTEND_URL,
+    credentials: true
+}));
 
-app.startServer(port)
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(limiter);
+
+// Routes
+app.use("/api", route);
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({
+        status: "fail",
+        message: `Cannot ${req.method} ${req.originalUrl}`,
+    });
+});
+
+// Error Handler
+app.use(errorHandler);
+
+// Start Server
+const port = Number(PORT) || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
